9c1fff575d0dd5834d6f9cb91c18c2c7afe328e7
Matteo <gnappo.1@live.it>
2021-07-15 19:20:52 -0400

    Include Route for About page

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 import QuizContainer from "../containers/QuizContainer"
 import Navbar from "./Navbar"
 import Home from "./Home"
+import About from "./About"
 import FutureGameList from "./FutureGameList"
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
         <Navbar />
         <Switch>
           <Route exact path="/" component={Home} /> 
+          <Route path="/about" component={About} /> 
           <Route path="/play" render={routeProps => <QuizContainer {...routeProps}/>}/> 
           <Route path="/future-games" render={routeProps => <FutureGameList {...routeProps}/>}/> 
         </Switch>
